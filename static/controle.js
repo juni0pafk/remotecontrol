c1cc67b5edc2412d84b549458281c36326d8f7ea
Patric Lacouth <lacouth@gmail.com>
2020-01-29 20:47:19 -0300

    add stop button

--- static/controle.js
+++ static/controle.js
@@ -2,15 +2,11 @@ let down = document.getElementById('down')
 let up = document.getElementById('up')
 let left = document.getElementById('left')
 let right = document.getElementById('right')
+let stop = document.getElementById('stop')
 
 down.addEventListener('touchstart',()=>fetch('/down_side'))
-// down.addEventListener('touchend',()=>fetch('/stop'))
-
 up.addEventListener('touchstart',()=>fetch('/up_side'))
-// up.addEventListener('touchend',()=>fetch('/stop'))
-
 left.addEventListener('touchstart',()=>fetch('/left_side'))
-// left.addEventListener('touchend',()=>fetch('/stop'))
-
 right.addEventListener('touchstart',()=>fetch('/right_side'))
-// right.addEventListener('touchend',()=>fetch('/stop'))
+
+stop.addEventListener('click',()=>fetch('/stop'))
