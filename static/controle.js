8c6de24759bc7e94090e892a461cbcc2628093fc
Patric Lacouth <lacouth@gmail.com>
2020-01-29 20:57:41 -0300

    change event touchstart to click

--- static/controle.js
+++ static/controle.js
@@ -4,9 +4,9 @@ let left = document.getElementById('left')
 let right = document.getElementById('right')
 let stop = document.getElementById('stop')
 
-down.addEventListener('touchstart',()=>fetch('/down_side'))
-up.addEventListener('touchstart',()=>fetch('/up_side'))
-left.addEventListener('touchstart',()=>fetch('/left_side'))
-right.addEventListener('touchstart',()=>fetch('/right_side'))
+down.addEventListener('click',()=>fetch('/down_side'))
+up.addEventListener('click',()=>fetch('/up_side'))
+left.addEventListener('click',()=>fetch('/left_side'))
+right.addEventListener('click',()=>fetch('/right_side'))
 
 stop.addEventListener('click',()=>fetch('/stop'))
