3c05e080819aea61633deb001000e4c443238707
Patric Lacouth <lacouth@gmail.com>
2020-01-31 10:13:21 -0300

    fixing resp in controle.js

--- static/js/controle.js
+++ static/js/controle.js
@@ -11,7 +11,7 @@ function atualiza_imagem() {
 
 down.addEventListener('click',() => 
     fetch('/down_side')
-    .then(resp => resp == 'true' ? atualiza_imagem() : "" )
+    .then(resp => resp.ok == 'true' ? atualiza_imagem() : "" )
 )
 
 
