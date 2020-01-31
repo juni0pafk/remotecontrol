194e4ea2bb751085a37e6c9e477f15839b483774
Patric Lacouth <lacouth@gmail.com>
2020-01-31 15:28:01 -0300

    a

--- static/js/controle.js
+++ static/js/controle.js
@@ -7,46 +7,31 @@ let img = document.getElementById('img')
 
 function atualiza_imagem() {
     fetch('/get_last_image')
-    .then(resp => {
-        resp.arrayBuffer().then(buffer => {
-            let base64Flag = 'data:image/jpeg;base64,';
-            let imageStr = arrayBufferToBase64(buffer);
-            img.src = base64Flag + imageStr
-        })
-    })
+    .then(resp => resp.blob())
+    .then(resp => img.src = URL.createObjectURL(resp))
 }
-
-function arrayBufferToBase64(buffer) {
-    var binary = '';
-    var bytes = [].slice.call(new Uint8Array(buffer));
-  
-    bytes.forEach((b) => binary += String.fromCharCode(b));
-  
-    return window.btoa(binary);
-  };
-  
 
 down.addEventListener('click',() => 
     fetch('/down_side')
-    .then(resp => resp.ok ? atualiza_imagem() : "" )
+    .then(() => atualiza_imagem())
 )
 
 
 up.addEventListener('click',() => 
     fetch('/up_side')
-    .then(atualiza_imagem())
+    .then(() => atualiza_imagem())
 )
 left.addEventListener('click',() => 
     fetch('/left_side')
-    .then(atualiza_imagem())
+    .then(() => atualiza_imagem())
 )
 
 right.addEventListener('click',() => 
     fetch('/right_side')
-    .then(atualiza_imagem())
+    .then(() => atualiza_imagem())
 )
 
 stop.addEventListener('click',() => 
     fetch('/stop')
-    .then(atualiza_imagem())
+    .then(() => atualiza_imagem())
 )
