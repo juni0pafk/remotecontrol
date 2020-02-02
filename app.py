a81bfceba405c1c5fa4de43e137c4990889fe8c5
Patric Lacouth <lacouth@gmail.com>
2020-02-02 17:10:37 -0300

    add download route

--- app.py
+++ app.py
@@ -13,24 +13,19 @@ import time
 import sys
 import signal
 
-def handler(signal, f):
-  print('CTRL-C pressed!')
-#   cam.release()
-  sys.exit(0)
-
-signal.signal(signal.SIGINT, handler)
-
+import pickle
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 CORS(app)
 
 
-#======= SETUP CAMERA=============
-cam = cv2.VideoCapture(0)
-
-img_counter = 0
+#============GLOBALS=============
+data = []
 last_filename = ""
+data_filename = ""
+sleep = .2
+actions = ['LEFT','RIGHT','FORWARD','BACKWARD']
 #=================================
 
 #Pinos do GPIO Raspberry 3 model B 
@@ -38,8 +33,6 @@ m11=26
 m12=19
 m21=21
 m22=20
-
-sleep = .2
 
 GPIO.setmode(GPIO.BCM) #RASPBERRY 3 MODEL B
 
@@ -78,16 +71,20 @@ def stop():
    GPIO.output(m21 , 0)
    GPIO.output(m22 , 0)
 
-def take_picture():
-   global last_filename
+def take_picture(action):
+   global last_filename,dados
    cam.open(0)
    cam.set(3,320) #Largura da imagem capturada
    cam.set(4,240) #Altura da imagem capturada
    (ret,frame) = cam.read()
    if ret:
-      last_filename = 'imagem_{}'.format(datetime.now())
-      last_filename = last_filename.replace(" ","_").replace(":","_").replace(".","_").replace("-","_") + '.png'
+      last_filename = 'imagem_' + get_timestamp() + '.png'
       cv2.imwrite('camera/' + last_filename,frame)
+      data.append((frame,action))
+
+def get_timestamp():
+   s = '{}'.format(datetime.now())
+   return s.replace(" ","_").replace(":","_").replace(".","_").replace("-","_")
 
 
 #Rota e função que renderiza o .html
@@ -99,8 +96,7 @@ def index():
 #Rota e função da esquerda
 @app.route('/left_side')
 def left_side():
-   take_picture()
-   data1="LEFT"
+   take_picture(actions[0])
    left()
    time.sleep(sleep)
    stop()
@@ -109,8 +105,7 @@ def left_side():
 #Rota e função da direita
 @app.route('/right_side')
 def right_side():
-   take_picture()
-   data1="RIGHT"
+   take_picture(actions[1])
    right()
    time.sleep(sleep)
    stop()
@@ -119,8 +114,7 @@ def right_side():
 #Rota e função da frente
 @app.route('/up_side')
 def up_side():
-   take_picture()
-   data1="FORWARD"
+   take_picture(actions[2])
    forward()
    time.sleep(sleep)
    stop()
@@ -129,8 +123,7 @@ def up_side():
 #Rota e função de trás
 @app.route('/down_side')
 def down_side():
-   take_picture()
-   data1="BACK"
+   take_picture(actions[3])
    backward()
    time.sleep(sleep)
    stop()
@@ -139,7 +132,6 @@ def down_side():
 #Rota e função de parada
 @app.route('/stop')
 def stop_route():
-   data1="STOP"
    stop()
    return  'true'
 
@@ -153,17 +145,26 @@ def image_route():
 def send_image(path):
     return send_from_directory('camera', path)
 
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
+@app.route('/save')
+def save_route():
+   global data_filename, data
+   if len(data):
+      data_filename = 'dados_{}_{}_imagens'.format(get_timestamp(),len(data))
+      file = open(data_filename,'w')
+      pickle.dump(data,'data/'+data_filename)
+      file.close()
+      return data_filename
+
+@app.route('/download')
+def download_route():
+   return send_from_directory('data',data_filename)
+
+def handler(signal, f):
+  print('CTRL-C pressed!')
+  cam.release()
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, handler)
 
 #NOT WORKING
 # TO DO: CRIAR UM TEMPLATE EM HTML+JS PARA LISTAR OS ARQUIVOS DA PASTA
